45f5fda52c84d6e46e1e100828b82292be41ac2f
JarenPOL1015 <jarapazm@espol.edu.ec>
2024-10-30 10:05:17 -0500

    Agg conexion al sv

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
  // Reemplaza con tu URL
- const databaseURL = 'https://prueba-8edf3-default-rtdb.firebaseio.com/coleccion.json'; 
+ const databaseURL = 'https://bd-dawm-3210b-default-rtdb.firebaseio.com/coleccion.json'; 
 
 let sendData = () => {
      // Obtén los datos del formulario
@@ -25,14 +25,82 @@ let sendData = () => {
     .then(result => {
         alert('Agradeciendo tu preferencia, nos mantenemos actualizados y enfocados en atenderte como mereces'); // Maneja la respuesta con un mensaje
         form.reset()
+
+        // Recuperación de datos
+        getData()
     })
     .catch(error => {
         alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
     });
 }
 
+let getData = async () => {
+
+    try {
+
+        // Realiza la petición fetch a la URL de la base de datos
+        const response = await fetch(databaseURL);
+
+        // Verifica si la respuesta es exitosa
+        if (!response.ok) {
+          alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+        }
+
+        // Convierte la respuesta en formato JSON
+        const data = await response.json();
+
+        if(data != null) {
+
+            // Cuente el número de suscriptores registrados por fecha a partir del objeto data
+            numero = 0;
+            for (const clave in data) {
+                numero++;
+            }
+
+            alert("Cantidad de subs: "+numero)
+            // END
+
+            // Genere y agregue filas de una tabla HTML para mostrar fechas y cantidades de suscriptores almacenadas 
+            const tbody = document.getElementById("subscribers");
+            tbody.innerHTML = '';
+            
+
+            // Itera sobre los datos para construir las filas de la tabla
+            Object.keys(data).forEach((key, index) => {
+                const item = data[key];
+
+                // Crea una nueva fila
+                const row = document.createElement("tr");
+
+                // Crea y agrega las celdas de la fila
+                const cellIndex = document.createElement("td");
+                cellIndex.textContent = index + 1;
+                row.appendChild(cellIndex);
+
+                const cellDay = document.createElement("td");
+                cellDay.textContent = item.saved;
+                row.appendChild(cellDay);
+
+                const cellEmail = document.createElement("td");
+                cellEmail.textContent = item.email;
+                row.appendChild(cellEmail);
+
+                // Añade la fila al tbody
+                tbody.appendChild(row);
+            });
+            // END
+
+        }
+
+      } catch (error) {
+        // Muestra cualquier error que ocurra durante la petición
+        alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+      }
+ }
+
 let ready = () => {
-    console.log('DOM está listo')
+    console.log('DOM está listo');
+    getData();
 }
 
 let loaded = (eventLoaded ) => {
